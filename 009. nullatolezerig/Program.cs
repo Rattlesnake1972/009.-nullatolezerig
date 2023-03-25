1d192d18949e1b7858540fe26c4ad18caed86c0e
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 21:24:33 +0100

    0-tól 1000-ig kiírja a számokat

---
+++ 009. nullatolezerig/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009.nullatolezerig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1001; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
